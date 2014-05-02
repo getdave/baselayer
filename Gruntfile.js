db7041a1dcf37315fc3bfbb12a316408668b5028
David <getdavemail@gmail.com>
2014-05-02 16:56:37 +0100

    Initial Gruntfile structure and basic dir structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,21 @@
 module.exports = function(grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  require('time-grunt')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    config: {
+        distDir: './dist',
+        cssSrc: './sass',
+        cssDist: './dist/css',
+        jsSrc: './js',
+        jsDist: './dist/js',
+        docsSrc: './docs',
+        docsDist: './dist/docs',
+        //packageBanner: grunt.file.read('banner.txt'),
+        fwFilename: '<%= pkg.name %>',
+    },
 
     sass: {
       options: {
@@ -12,23 +27,76 @@ module.exports = function(grunt) {
         },
         files: {
           'css/app.css': 'scss/app.scss'
-        }        
+        }
       }
     },
 
     watch: {
+      options: {
+        livereload: true,
+      },
+
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
         files: 'scss/**/*.scss',
         tasks: ['sass']
+      },
+
+      templates: {
+          files: ['<%= jekyll.templates.options.src %>/**/*.html'],
+          tasks: ['copy:templates', 'jekyll:templates']
+      },
+    },
+
+    jekyll: {
+      options: {
+        //bundleExec: true,
+
+      },
+      templates: {
+        options: {
+          src : 'templates/src',
+          dest: 'templates/dest'
+        }
+      },
+    },
+
+    connect: {
+      options: {
+        open: true,
+        keepalive: false
+      },
+      templates: {
+        options: {
+          port: 9000,
+          base: '<%= jekyll.templates.options.dest %>'
+        }
       }
-    }
+    },
+
+
+    copy: {
+          options: {
+              flatten: true
+          },
+          templates: { // copy all assets into templates source dir
+              src: [
+                  'css/app.css'
+              ],
+              dest: '<%= jekyll.templates.options.src %>/assets/'
+          }
+      },
   });
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['build','watch']);
+
+  grunt.registerTask('templates', [
+      'copy:templates',
+      'jekyll:templates',
+      'connect:templates',
+      'default'
+  ]);
 }
