12ec027533f3f7f2c4c844d9930608e29f4b0469
David <getdavemail@gmail.com>
2014-05-08 14:14:34 +0100

    Implement basic oldie compatibility via Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,11 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   // load all grunt tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   require('time-grunt')(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     config: {
       distDir: './dist',
       cssSrc: './sass',
@@ -17,63 +18,107 @@ module.exports = function(grunt) {
       fwFilename: '<%= pkg.name %>',
     },
 
-
+    // Watch files for changes, run tasks and reload browser
     watch: {
       options: {
         livereload: true,
       },
 
-      grunt: { files: ['Gruntfile.js'] },
+      grunt: {
+        files: ['Gruntfile.js']
+      },
 
       sass: {
         files: 'scss/**/*.scss',
         tasks: [
-        'sass',
-          //'pixrem' // required for IE only
-          ]
-        },
-
-        js: {
-          files: ['js/baselayer.js','js/site.js'],
-          tasks: ['uglify']
-        },
-
-        templates: {
-          files: ['<%= jekyll.templates.options.src %>/**/*.html'],
-          tasks: ['copy:templates', 'jekyll:templates']
-        },
-
-        docs: {
-          files: ['<%= jekyll.docs.options.src %>/**/*.html'],
-          tasks: ['copy:docs', 'jekyll:docs']
-        },
+          'css'
+        ]
       },
-      sass: {
+
+      js: {
+        files: ['js/baselayer.js', 'js/site.js'],
+        tasks: ['uglify']
+      },
+
+      templates: {
+        files: ['<%= jekyll.templates.options.src %>/**/*.html'],
+        tasks: ['copy:templates', 'jekyll:templates']
+      },
+
+      docs: {
+        files: ['<%= jekyll.docs.options.src %>/**/*.html'],
+        tasks: ['copy:docs', 'jekyll:docs']
+      },
+    },
+
+    // Connect - spin up local server and open in default system browser
+    connect: {
+      options: {
+        open: true,
+        keepalive: false,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost',
+        // hostname: '0.0.0.0'
+
+      },
+      templates: {
         options: {
-          includePaths: [
-          'bower_components/foundation/scss',
-          'scss/baselayer'
-          ]
-        },
-        dist: {
-          options: {
-            outputStyle: 'expanded'
-          },
-          files: {
-            'css/site.css': 'scss/site.scss',
-          //'css/site-ie.css': 'scss/site-ie.scss'
+          port: 9000,
+          base: '<%= jekyll.templates.options.dest %>'
+        }
+      },
+      docs: {
+        options: {
+          port: 9001,
+          base: '<%= jekyll.docs.options.dest %>'
         }
       }
     },
+
+    // Sass - build CSS files from SCSS using grunt-sass (via libsass for speed!)
+    sass: {
+      options: {
+        includePaths: [
+          'bower_components/foundation/scss',
+          'scss/baselayer'
+        ]
+      },
+      dist: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'css/site.css': 'scss/site.scss'
+        }
+      }
+    },
+
+    // Pixrem - provide "px" fallback for "rem" units and create site-oldie.css file
     pixrem: {
       options: {
         //rootvalue: '16px'
       },
       dist: {
         src: 'css/site.css',
-        dest: 'css/site-ie.css'
+        dest: 'css/site-oldie.css'
       }
     },
+
+
+    // Legacssy - flatten MQ's in site-oldie.css
+    legacssy: {
+      dist: {
+        options: {
+          // Include only styles for a screen 800px wide
+          legacyWidth: 60
+        },
+        files: {
+          'css/site-oldie.css': 'css/site-oldie.css',
+        },
+      },
+    },
+
+    // Uglify - process all JS
     uglify: {
       options: {
 
@@ -81,14 +126,14 @@ module.exports = function(grunt) {
       dist: {
         files: {
           'js/conditional/superfish.min.js': [
-          'bower_components/superfish/dist/js/hoverIntent.js',
-          'bower_components/superfish/dist/js/superfish.js'
+            'bower_components/superfish/dist/js/hoverIntent.js',
+            'bower_components/superfish/dist/js/superfish.js'
           ],
 
           'js/vendor/modernizr.min.js': ['bower_components/modernizr/modernizr.js'],
           'js/vendor/foundation.min.js': [
-          'bower_components/fastclick/lib/fastclick.js',
-          'bower_components/foundation/js/foundation/foundation.js',
+            'bower_components/fastclick/lib/fastclick.js',
+            'bower_components/foundation/js/foundation/foundation.js',
 
             // Include all or...
             //'bower_components/foundation/js/foundation/*.js'
@@ -110,60 +155,44 @@ module.exports = function(grunt) {
             //'bower_components/foundation/js/foundation/foundation.equalizer.js',
             //'bower_components/foundation/js/foundation/foundation.orbit.js',
             //'bower_components/foundation/js/foundation/foundation.tab.js'
-            ],
-            'js/baselayer.min.js': ['js/baselayer.js'],
-          }
-        },
+          ],
+          'js/baselayer.min.js': ['js/baselayer.js'],
+        }
       },
-      jekyll: {
-        options: {
-        //bundleExec: true,
+    },
+
+
+    // Jekyll - build Documentation and example Templates into static sites
+    jekyll: {
+      options: {
 
       },
       templates: {
         options: {
-          src : 'templates/src',
+          src: 'templates/src',
           dest: 'templates/dest'
         }
       },
       docs: {
         options: {
-          src : 'docs/src',
+          src: 'docs/src',
           dest: 'docs/dest'
         }
       },
     },
 
-    connect: {
-      options: {
-        open: true,
-        keepalive: false
-      },
-      templates: {
-        options: {
-          port: 9000,
-          base: '<%= jekyll.templates.options.dest %>'
-        }
-      },
-      docs: {
-        options: {
-          port: 9000,
-          base: '<%= jekyll.docs.options.dest %>'
-        }
-      }
-    },
 
-
+    // Copy - copy assets into required destination ready for use in Docs and Templates
     copy: {
       options: {
         flatten: false
       },
       templates: { // copy all assets into templates source dir
         src: [
-        'css/*.css',
-        'js/**/*.js',
-        'images/**/*',
-        'fonts/**/*',
+          'css/*.css',
+          'js/**/*.js',
+          'images/**/*',
+          'fonts/**/*',
         ],
         dest: '<%= jekyll.templates.options.src %>/assets/'
       },
@@ -172,24 +201,59 @@ module.exports = function(grunt) {
         dest: '<%= jekyll.docs.options.src %>/assets/'
       }
     },
-    
+
+
+    // Clean - ensure no legacy files are left around
+    clean: {
+      css: [
+        'css/*.css'
+      ],
+      templates: [
+        'templates/dest/',
+        'templates/src/assets/',
+      ],
+      docs: [
+        'docs/src/assets/',
+        'docs/dest/',
+      ],
+    },
+
   });
 
+  // Default - build and then start watching
+  grunt.registerTask('default', ['build', 'watch']);
 
-grunt.registerTask('build', ['sass','pixrem','uglify']);
-grunt.registerTask('default', ['build','watch']);
 
-grunt.registerTask('templates', [
-  'copy:templates',
-  'jekyll:templates',
-  'connect:templates',
-  'default'
+  // Master Build task - prepare for deployment
+  grunt.registerTask('build', [
+    'clean',
+    'css',
+    'uglify'
   ]);
 
-grunt.registerTask('docs', [
-  'copy:docs',
-  'jekyll:docs',
-  'connect:docs',
-  'default'
+  // CSS - process CSS
+  grunt.registerTask('css', [
+    'clean:css',
+    'sass',
+    'pixrem',
+    'legacssy'
+  ]);
+
+  // Templates - build example Templates static site
+  grunt.registerTask('templates', [
+    'clean:templates',
+    'copy:templates',
+    'jekyll:templates',
+    'connect:templates',
+    'default'
+  ]);
+
+  // Docs - build Component Documentation static site
+  grunt.registerTask('docs', [
+    'clean:docs',
+    'copy:docs',
+    'jekyll:docs',
+    'connect:docs',
+    'default'
   ]);
 }
